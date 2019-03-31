4e1633bac714878ab19ce29ecdbebae1658843be
mxy <xymasdu@163.com>
2019-03-31 21:40:29 +0800

    modules

--- gen_bimpm_cofig.py
+++ gen_bimpm_cofig.py
@@ -7,16 +7,21 @@ import yaml
 rv = {}
 
 # Path
-rv['meta_dir'] = 'data/preprocessed/QQP/'
-rv['train_file'] = 'data/preprocessed/QQP/train_spacy.jsonl'
-rv['dev_file'] = 'data/preprocessed/QQP/dev_spacy.jsonl'
-rv['test_file'] = 'data/preprocessed/QQP/test_spacy.jsonl'
-rv['result_dir'] = 'data/outputs/QQP/'
+# rv['meta_dir'] = 'data/preprocessed/QQP/'
+# rv['train_file'] = 'data/preprocessed/QQP/train_spacy.jsonl'
+# rv['dev_file'] = 'data/preprocessed/QQP/dev_spacy.jsonl'
+# rv['test_file'] = 'data/preprocessed/QQP/test_spacy.jsonl'
+# rv['result_dir'] = 'data/outputs/QQP/'
+# rv['embedding_file'] = '/home/fanyixing/users/mxy/coqa-baselines/wordvecs/glove.840B.300d.txt'
+rv['meta_dir'] = 'data/preprocessed/QQPdebug/'
+rv['train_file'] = 'data/preprocessed/QQPdebug/train_spacy.jsonl'
+rv['dev_file'] = 'data/preprocessed/QQPdebug/dev_spacy.jsonl'
+rv['test_file'] = 'data/preprocessed/QQPdebug/test_spacy.jsonl'
+rv['result_dir'] = 'data/outputs/QQPdebug/'
 rv['embedding_file'] = ''
 
-
 # General
-rv['batch_size'] = 32
+rv['batch_size'] = 64
 rv['epoches'] = 10
 rv['num_workers'] = 2
 rv['use_cuda'] = True
@@ -26,9 +31,9 @@ rv['max_seq_length'] = 50
 rv['dropout'] = 0.1
 rv['num_perspective'] = 20
 rv['use_char_emb'] = True
-rv['char_hidden_size'] = 5
-rv['network'] = 'test_model_1'
-rv['hidden_size'] = 64
+rv['char_hidden_size'] = 50
+rv['network'] = 'bimpm'
+rv['hidden_size'] = 100
 rv['rnn_layers'] = 2
 rv['classes'] = 2
 rv['word_dim'] = 300
