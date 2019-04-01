b9d60ebd4be7d5a074424c02b70f909ffc024f19
LixinSu <sulixinict@gmail.com>
2019-04-01 22:28:31 +0800

    add gen esim config

--- gen_esim_config.py
+++ gen_esim_config.py
@@ -7,16 +7,17 @@ import yaml
 rv = {}
 
 # Path
-rv['meta_dir'] = 'data/preprocessed/QQPdebug/'
-rv['train_file'] = 'data/preprocessed/QQPdebug/train_spacy.jsonl'
-rv['dev_file'] = 'data/preprocessed/QQPdebug/train_spacy.jsonl'
-rv['test_file'] = 'data/preprocessed/QQPdebug/test_spacy.jsonl'
-rv['result_dir'] = 'data/outputs/QQPdebug/'
+rv['meta_dir'] = 'data/preprocessed/QQP/'
+rv['train_file'] = 'data/preprocessed/QQP/train_spacy.jsonl'
+rv['dev_file'] = 'data/preprocessed/QQP/train_spacy.jsonl'
+rv['test_file'] = 'data/preprocessed/QQP/test_spacy.jsonl'
+rv['result_dir'] = 'data/outputs/QQP/'
 # rv['embedding_file'] = ''
-rv['embedding_file'] = '/home/fanyixing/users/mxy/coqa-baselines/wordvecs/glove.42B.300d.txt'
+rv['embedding_file'] = '/data/sulixin/research/longspan/RCZoo/data/embeddings/glove.840B.300d.txt'
+rv['length_limit'] = 1000
 
 # General
-rv['batch_size'] = 64
+rv['batch_size'] = 32
 rv['epoches'] = 20
 rv['num_workers'] = 2
 rv['use_cuda'] = True
@@ -26,12 +27,11 @@ rv['network'] = 'esim'
 rv['min_cnt'] = 0
 rv['word_dim'] = 300
 rv['hidden_size'] = 100
-rv['classes'] = 2
+rv['classes'] = 3
 rv['dropout'] = 0.1
 rv['optimizer'] = 'adamax'
 rv['weight_decay'] = 0
 rv['grad_clipping'] = 10
-
 
 #rv['max_seq_length'] = 50
 #rv['max_word_length'] = 16
