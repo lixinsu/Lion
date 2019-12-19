1c1603ecac47fb3516ee9aa330ef50eb462b3da2
LixinSu <sulixinict@gmail.com>
2019-12-19 11:30:34 +0800

    fix eval and predict;set fix_embedding false

--- lion/training/trainer.py
+++ lion/training/trainer.py
@@ -21,7 +21,7 @@ DEFAULTS = {'batch_size': 32,
             'epoches': 20,
             'use_cuda': False,
             'parallel': False,
-            'num_workers': 2,
+            'num_workers': 0,
             'length_limit': 512,
             'optimizer': 'adamax',
             'learning_rate': 0.0001,
@@ -31,7 +31,7 @@ DEFAULTS = {'batch_size': 32,
             'patience': 5,
             'use_elmo': None,  # 'only' or 'concat'
             'embedding_file': None,
-            'fix_embeddings': True,
+            'fix_embeddings': False,
             'sorted': True,
             'max_A_len': None,
             'max_B_len': None}
@@ -52,12 +52,6 @@ def check_fill_parameters(params, split='train'):
     if split == 'train':
         if 'train_file' not in params or 'dev_file' not in params:
             raise ValueError("Train Mode must specify train_file and dev_file in config file")
-    elif split == 'dev':
-        if 'dev_file' not in params:
-            raise ValueError("Evaluate Mode must specify 'dev_file' in config file")
-    elif split == 'test':
-        if 'test_file' not in params:
-            raise ValueError("Predict Mode must specify 'test_file' in config file")
     return fill_default_parameters(params)
 
 
@@ -72,13 +66,16 @@ def train():
         if vocab_name == 'word' and 'vocab_file' in params and params['vocab_file'] is not None:
             # Load vocab from existing file
             vocab_ = Dictionary.load_txt(params.vocab_file)
+        elif vocab_name == 'labelmapping':
+            # Load vocab from self-create file
+            vocab_ = json.load(open(osp.join(params.meta_dir, '{}.json'.format(vocab_name))))
         else:
             # Load vocab from self-create file
             vocab_ = Dictionary.load_json(osp.join(params.meta_dir, '{}.json'.format(vocab_name)),
                                           min_cnt=params.min_cnt)
         params.update({'{}_dict_size'.format(vocab_name): len(vocab_)})
         params.update({'{}_dict'.format(vocab_name): vocab_})
-    params.update({'classes': len(params['labelmapping_dict'])})
+    params.update({'classes': len(set(params['labelmapping_dict'].values()))})
     logger.info('\n' + str(params))
     train_dataset = LionDataset(params.train_file, params)
     # pre-compute num train steps for `bert`
@@ -105,29 +102,29 @@ def train():
     logger.info('Best metric:{}'.format(best_metric))
 
 
-def evaluate():
+def evaluate(output_dir, dev_file):
     """Evaluate Model."""
-    model, params = load_model(osp.join(args.output_dir, MODEL_FILE))
+    model, params = load_model(osp.join(output_dir, MODEL_FILE))
     params = check_fill_parameters(params, split='dev')
-    dev_dataset = LionDataset(params.dev_file, params)
+    dev_dataset = LionDataset(dev_file, params)
     dev_loader = prepare_loader(dev_dataset, params, split='dev')
     result = model.evaluate_epoch(dev_loader)
     logger.info("Acc : {}".format(result['acc']))
 
 
-def predict():
+def predict(output_dir, test_file):
     """Predict."""
-    model, params = load_model(osp.join(args.output_dir, MODEL_FILE))
+    model, params = load_model(osp.join(output_dir, MODEL_FILE))
     params = check_fill_parameters(params, split='test')
-    test_dataset = LionDataset(params.test_file, args)
-    test_loader = prepare_loader(test_dataset, args, split='dev')
+    test_dataset = LionDataset(test_file, params)
+    test_loader = prepare_loader(test_dataset, params, split='dev')
     rv = model.predict_epoch(test_loader)
     id2label = {}
-    for label, index in json.load(open(osp.join(args.meta_dir, 'labelmapping.json'))).items():
+    for label, index in json.load(open(osp.join(params.meta_dir, 'labelmapping.json'))).items():
         id2label[index] = label
     for key, value in rv.items():
         rv[key] = id2label[value]
-    predict_file = osp.join(args.output_dir, 'predictions.json')
+    predict_file = osp.join(params.output_dir, 'predictions.json')
     if osp.isfile(predict_file):
         logger.warning('Will overwrite original predictions')
     json.dump(rv, open(predict_file, 'w'))
@@ -145,6 +142,8 @@ if __name__ == '__main__':
     parser.add_argument('--evaluate', action='store_true', help='Evaluating')
     parser.add_argument('--predict', action='store_true', help='Predicting')
     parser.add_argument('--output_dir', type=str, required=True, help='Output path')
+    parser.add_argument('--test_file', type=str, help='Test file')
+    parser.add_argument('--dev_file', type=str, help='dev file')
     args = parser.parse_args()
     config_file = osp.join(args.output_dir, 'params.yaml')
     if not osp.isfile(config_file):
@@ -154,8 +153,10 @@ if __name__ == '__main__':
         logger.info('Save model in {}'.format(args.output_dir))
         train()
     elif args.evaluate:
-        evaluate()
+        logger = prepare_logger(osp.join(args.output_dir, 'evaluate.log'))
+        evaluate(args.output_dir, args.dev_file)
     elif args.predict:
-        predict()
+        logger = prepare_logger(osp.join(args.output_dir, 'predict.log'))
+        predict(args.output_dir, args.test_file)
     else:
         raise ValueError("At least one of train evaluate predict shoud be true")
